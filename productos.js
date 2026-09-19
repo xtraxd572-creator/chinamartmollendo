3036abd232637cb28060c393d59677e60595f7e7
leo <xtraxd572@gmail.com>
2026-09-19 09:55:13 -0500

    Remove destacado property from product object

--- productos.js
+++ productos.js
@@ -49,7 +49,6 @@ const productosChinaMart = [
         mensaje: "Hola, quisiera consultar por la Mochila Escolar Capibara",
         codigo: "",
         stock: null
-        destacado: true,
     },
     {
     nombre: "Mascarillas Faciales BIOAQUA Koala y Zorrito",
